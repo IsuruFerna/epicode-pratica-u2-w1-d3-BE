2faecf1b5d592c8b101d80c3be0dfa59434b967d
IsuruFerna <isurumadushan.ferna@gmail.com>
2024-01-10 16:25:32 +0100

    menu fixed

--- src/main/java/com/epicode/u2w1d1/entities/Menu.java
+++ src/main/java/com/epicode/u2w1d1/entities/Menu.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +15,23 @@ public class Menu {
     private List<Pizza> pizzaList;
     private List<Topping> toppingList;
     private List<Drink> drinkList;
+
+    @Override
+    public String toString() {
+        System.out.println("---------- PIZZA ------------");
+        pizzaList.stream().forEach(pizza -> {
+            String ingredients = pizza.getIngredients().stream().map(Topping::getName).collect(Collectors.joining(", "));
+            System.out.println(pizza.getName() + " (" + ingredients + ") " + "calories: " + pizza.getCalories() + " price: " + pizza.getPrice());
+        });
+
+        System.out.println("---------- TOPPING ------------");
+        toppingList.forEach(topping -> {
+            System.out.println(topping.getName() + " " + topping.getCalories() + " " + topping.getPrice());
+        });
+        System.out.println("---------- DRINK ------------");
+        drinkList.forEach(drink -> System.out.println(drink.getName() + "(" + drink.getVolume() + ")" + " " + drink.getCalories() + " " + drink.getPrice()));
+
+        return "";
+    }
 }
 
